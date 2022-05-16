f4343c7d261a19ecc3c0af0aa4b68e108b48e7ac
Mika Herrmann <nh273@stud.uni-heidelberg.de>
2022-05-16 10:21:30 +0200

    Added Random Generator

--- include/ImageGenerator/Generators.hpp
+++ include/ImageGenerator/Generators.hpp
@@ -1,17 +1,42 @@
 #ifndef GENERATOR_HPP
 #define GENERATOR_HPP
 
+#include <random>
+
 using rgb = unsigned char;
 
 class GeneratorManager;
+class Renderer;
 
 class Generator {
     protected:
-        rgb* data;
+        Renderer* renderer;
         int width, height, size;
     
     public:
         Generator(const GeneratorManager& main);
+
+        virtual bool generateImage() = 0;
+};
+
+class RandomGenerator : public virtual Generator {
+    private:
+        long seed;
+        std::mt19937_64 gen;
+
+    public:
+        /**
+         * Initializes a new Random Generator with the given seed
+         * @param main The instance of the manager
+         * @param seed The seed for the RNG to use
+         */ 
+        RandomGenerator(const GeneratorManager& main, const long& seed);
+
+        /**
+         * Initializes a new Random Generator with the standard seed (system time)
+         * @param main The instance of the manager
+         */ 
+        RandomGenerator(const GeneratorManager& main);
 
         virtual bool generateImage() = 0;
 };
