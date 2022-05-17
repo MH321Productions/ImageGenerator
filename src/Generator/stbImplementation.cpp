b3e1e29ebc633788b39d53eeeb899125bd958391
Mika Herrmann <nh273@stud.uni-heidelberg.de>
2022-05-17 14:58:27 +0200

    Added custom compress function for PNGs

--- src/Generator/stbImplementation.cpp
+++ src/Generator/stbImplementation.cpp
@@ -1,2 +1,9 @@
+unsigned char * pngCompress(unsigned char *data, int data_len, int *out_len, int quality);
+
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STBIW_ZLIB_COMPRESS pngCompress
 #include "stb_image_write.h"
+
+unsigned char * pngCompress(unsigned char *data, int data_len, int *out_len, int quality) {
+
+}
