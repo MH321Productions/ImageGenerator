6f42165bf0994a711aaefaf078b9597d15a2cd21
Mika Herrmann <nh273@stud.uni-heidelberg.de>
2022-05-25 12:14:51 +0200

    Added Test for Transparent Genrator

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,12 +7,10 @@ using namespace std;
 int main(int anzahl, char* args[]) {
     cout << "Hello World" << endl;
 
-    //Test
+    //Tests
     GeneratorManager man(1920, 1080, "Test.png", PNG);
     //RandomGenerator rand(man, 20);
-    //man.setGenerator(&rand);
-
-    ColorGenerator color(man);
-    //man.setGenerator(&color);
+    //ColorGenerator color(man);
+    TransparentGenerator trans(man);
     man.generateImage();
 }
