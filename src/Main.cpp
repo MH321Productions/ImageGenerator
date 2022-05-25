92ee7683aef9cf2cb5682270d00fbef4c6c7eab3
Mika Herrmann <nh273@stud.uni-heidelberg.de>
2022-05-25 09:16:38 +0200

    Added first test

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include "ImageGenerator/ImageGenerator.hpp"
+#include "ImageGenerator/Generators.hpp"
 
 using namespace std;
 
 int main(int anzahl, char* args[]) {
     cout << "Hello World" << endl;
+
+    //Test
+    GeneratorManager man(1920, 1080, "Test.png", PNG);
+    RandomGenerator rand(man, 20);
+    man.setGenerator(&rand);
+    man.generateImage();
 }
