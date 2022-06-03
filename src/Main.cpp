7fd0a1b6abf8ef62b6e1ed988ac81c0e237fc454
Mika Herrmann <nh273@stud.uni-heidelberg.de>
2022-06-03 10:43:37 +0200

    Added test for Mandelbrot generator

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,7 +10,8 @@ int main(int anzahl, char* args[]) {
     //Tests
     GeneratorManager man(1920, 1080, "Test.png", PNG);
     //RandomGenerator rand(man, 20);
-    ColorGenerator color(man, 0xFFFFFFFF);
+    //ColorGenerator color(man, 0xFFFFFFFF);
     //TransparentGenerator trans(man);
+    MandelbrotGenerator mandelbrot(man, -2.5, 1.1, 3.5, 132);
     man.generateImage();
 }
